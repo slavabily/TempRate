e859f7c8282c2221dc54dd2f6fd4087c4cb4fff4
slava bily <slavabily@me.com>
2020-12-07 16:02:02 +0200

    base app structure is created from HeartRate template

--- TempRate WatchKit Extension/ContentView.swift
+++ TempRate WatchKit Extension/ContentView.swift
@@ -6,12 +6,99 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .padding()
+    
+    private var healthStore = HKHealthStore()
+    let heartRateQuantity = HKUnit(from: "count/min")
+        
+    @State private var value = 0
+    
+    func start() {
+        authorizeHealthKit()
+        startHeartRateQuery(quantityTypeIdentifier: .heartRate)
     }
+    
+    func authorizeHealthKit() {
+        
+        // Used to define the identifiers that create quantity type objects.
+        let healthKitTypes: Set = [
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!]
+        // Requests permission to save and read the specified data types.
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { _, _ in }
+    }
+    
+    private func process(_ samples: [HKQuantitySample], type: HKQuantityTypeIdentifier) {
+        // variable initialization
+        var lastHeartRate = 0.0
+        
+        // cycle and value assignment
+        for sample in samples {
+            if type == .heartRate {
+                lastHeartRate = sample.quantity.doubleValue(for: heartRateQuantity)
+            }
+            
+            self.value = Int(lastHeartRate)
+        }
+    }
+    
+    private func startHeartRateQuery(quantityTypeIdentifier: HKQuantityTypeIdentifier) {
+            
+            // We want data points from our current device
+            let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
+            
+            // A query that returns changes to the HealthKit store, including a snapshot of new changes and continuous monitoring as a long-running query.
+            let updateHandler: (HKAnchoredObjectQuery, [HKSample]?, [HKDeletedObject]?, HKQueryAnchor?, Error?) -> Void = {
+                query, samples, deletedObjects, queryAnchor, error in
+                
+             // A sample that represents a quantity, including the value and the units.
+            guard let samples = samples as? [HKQuantitySample] else {
+                return
+            }
+                
+            self.process(samples, type: quantityTypeIdentifier)
+
+            }
+            
+            // It provides us with both the ability to receive a snapshot of data, and then on subsequent calls, a snapshot of what has changed.
+            let query = HKAnchoredObjectQuery(type: HKObjectType.quantityType(forIdentifier: quantityTypeIdentifier)!, predicate: devicePredicate, anchor: nil, limit: HKObjectQueryNoLimit, resultsHandler: updateHandler)
+            
+            query.updateHandler = updateHandler
+            
+            // query execution
+            
+            healthStore.execute(query)
+        }
+    
+    var body: some View {
+            VStack{
+                HStack{
+                    Text("❤️")
+                        .font(.system(size: 50))
+                    Spacer()
+
+                }
+                
+                HStack{
+                    Text("\(value)")
+                        .fontWeight(.regular)
+                        .font(.system(size: 70))
+                    
+                    Text("BPM")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.red)
+                        .padding(.bottom, 28.0)
+                    
+                    Spacer()
+                    
+                }
+
+            }
+            .padding()
+            .onAppear(perform: start)
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
